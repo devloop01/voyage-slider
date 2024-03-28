93268f7d3369fb99c1e978f64cb89d0fe4899a58
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-03-28 21:16:35 +0530

    fix stying and dom

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ const buttons = {
   next: document.querySelector(".slider--btn__next"),
 };
 
-const slides = document.querySelectorAll(".slide");
+const slides = [...document.querySelectorAll(".slide")];
+const slideInfos = [...document.querySelectorAll(".slide-info")];
 
-slides.forEach((slide) => {
-  const slideWrapper = slide.parentElement;
+slides.forEach((slide, index) => {
   const slideInner = slide.querySelector(".slide__inner");
-  const slideInfoInner = slideWrapper.querySelector(".slide-info__inner");
+  const slideInfoInner = slideInfos[index].querySelector(".slide-info__inner");
 
   tilt(slide, { target: [slideInner, slideInfoInner] });
 });
@@ -20,8 +20,6 @@ buttons.next.addEventListener("click", swapCards("right"));
 
 function swapCards(direction) {
   return () => {
-    const slidesWrappper = document.querySelector(".slides__wrapper");
-
     const slideCurrent = document.querySelector(".slide[data-current]");
     const slidePrevious = document.querySelector(".slide[data-previous]");
     const slideNext = document.querySelector(".slide[data-next]");
@@ -30,38 +28,46 @@ function swapCards(direction) {
     const slideWrapperPrevious = slidePrevious.parentElement;
     const slideWrapperNext = slideNext.parentElement;
 
-    const slideInfoCurrent = slideWrapperCurrent.querySelector(".slide-info");
-    const slideInfoPrevious = slideWrapperPrevious.querySelector(".slide-info");
-    const slideInfoNext = slideWrapperNext.querySelector(".slide-info");
-
-    slidesWrappper.setAttribute("data-direction", direction);
+    const slideInfoCurrent = document.querySelector(
+      ".slide-info[data-current]",
+    );
+    const slideInfoPrevious = document.querySelector(
+      ".slide-info[data-previous]",
+    );
+    const slideInfoNext = document.querySelector(".slide-info[data-next]");
 
     slideCurrent.removeAttribute("data-current");
     slidePrevious.removeAttribute("data-previous");
     slideNext.removeAttribute("data-next");
 
     slideInfoCurrent.removeAttribute("data-current");
+    slideInfoPrevious.removeAttribute("data-previous");
+    slideInfoNext.removeAttribute("data-next");
 
     if (direction === "right") {
-      slideWrapperNext.style.zIndex = "30";
-      slideWrapperCurrent.style.zIndex = "20";
+      slideWrapperNext.style.zIndex = "20";
+      slideWrapperCurrent.style.zIndex = "30";
       slideWrapperPrevious.style.zIndex = "10";
 
       slideCurrent.setAttribute("data-previous", "");
       slidePrevious.setAttribute("data-next", "");
       slideNext.setAttribute("data-current", "");
 
+      slideInfoCurrent.setAttribute("data-previous", "");
       slideInfoNext.setAttribute("data-current", "");
+      slideInfoPrevious.setAttribute("data-next", "");
     } else if (direction === "left") {
-      slideWrapperPrevious.style.zIndex = "30";
-      slideWrapperCurrent.style.zIndex = "20";
+      slideWrapperPrevious.style.zIndex = "20";
+      slideWrapperCurrent.style.zIndex = "30";
       slideWrapperNext.style.zIndex = "10";
 
       slideCurrent.setAttribute("data-next", "");
       slidePrevious.setAttribute("data-current", "");
       slideNext.setAttribute("data-previous", "");
 
+      slideInfoCurrent.setAttribute("data-next", "");
       slideInfoPrevious.setAttribute("data-current", "");
+      slideInfoNext.setAttribute("data-previous", "");
     }
   };
 }
